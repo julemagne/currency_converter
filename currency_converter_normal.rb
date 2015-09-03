f5d82fbb758a9545ced355b9cbf64071835049c9
Julie David <julie.angela.david@gmail.com>
2015-09-03 15:41:18 -0400

    Take user input.

--- currency_converter_normal.rb
+++ currency_converter_normal.rb
@@ -3,5 +3,6 @@ require './Currency'
 require './CurrencyConverter'
 
 puts "Enter the amount and currency that you would like to convert from."
-from = gets.chomp.to_s
+from = gets.chomp.to_sym
 puts "Enter the currency you wouldl ike to convert to."
+to = gets.chomp.to_sym

4820de19dab4f613004c3dd4341f01125fcf0a37
Julie David <julie.angela.david@gmail.com>
2015-09-06 08:27:53 -0400

    Class CurrencyTrader is initialized with two CurrencyConverter objects and a starting currency.

---
+++ CurrencyTrader.rb
@@ -0,0 +1,16 @@
+class CurrencyTrader
+
+  def initialize(converter, converter2, currency)
+    @current = converter
+    @old = converter2
+    @currency = currency
+    end
+
+  def invest
+
+  end
+
+
+
+
+end
--- currency_converter_normal.rb
+++ currency_converter_normal.rb
@@ -1,8 +1,16 @@
 # currency_converter_normal
 require './Currency'
 require './CurrencyConverter'
+require './CurrencyTrader'
 
 # No commas, please.
 converter = CurrencyConverter.new(USD: 1.0, EUR: 0.89805, GBP: 0.65534, JPY: 119.959, THB: 32.8280)
+converter2 = CurrencyConverter.new()
+
+first_currency_object = Currency.new(value, cc)
 
 converter.convert(first_currency_object, currency)
+
+trader = CurrencyTrader.new(converter, converter2, currency)
+
+trader.invest
